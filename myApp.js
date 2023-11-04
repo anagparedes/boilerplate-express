aca22d756ff0cdac6a45e5fa41357549ee1a2b2f
Ana Paredes <49009311+anagparedes@users.noreply.github.com>
2023-11-04 23:59:17 +0000

    serve an html file

--- myApp.js
+++ myApp.js
@@ -1,10 +1,14 @@
 let express = require('express');
 let app = express();
+let path = __dirname + '/views/index.html'
+
+// app.get("/",function(req, res) {
+//     res.send('Hello Express');
+//   })
 
 app.get("/",function(req, res) {
-    res.send('Hello Express');
+    res.sendFile(path)
   })
-
 
 
 
