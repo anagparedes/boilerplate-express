d17ea8465889b5f1b05142c4da8c32038ec11eff
Ana Paredes <49009311+anagparedes@users.noreply.github.com>
2023-11-06 21:17:56 +0000

    testing

--- myApp.js
+++ myApp.js
@@ -9,9 +9,14 @@ require('dotenv').config()
 //     res.send('Hello Express');
 //   })
 
-app.get("/json", function(req, res) {
-  process.env.MESSAGE_STYLE === "uppercase" ? res.json({"message": "HELLO JSON"}) : res.json({"message": "Hello json"});
-  })
+// app.get("/json", function(req, res) {
+//   process.env.MESSAGE_STYLE === "uppercase" ? res.json({"message": "HELLO JSON"}) : res.json({"message": "Hello json"});
+//   })
+app.get('/json', (req, res) => {
+  process.env.MESSAGE_STYLE === 'uppercase' ? 
+    res.json({ "message": "HELLO JSON" }):
+    res.json({ "message": "Hello json" })
+})
 
 // app.use("/public", express.static(style_path))
 
