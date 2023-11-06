9a122bbcbd757f0fdc76eb2c50292d38e1ec34f3
Ana Paredes <49009311+anagparedes@users.noreply.github.com>
2023-11-06 21:01:58 +0000

    Use the .env File

--- myApp.js
+++ myApp.js
@@ -3,12 +3,14 @@ let app = express();
 let path = __dirname + '/views/index.html'
 let style_path = __dirname + '/public'
 
+require('dotenv').config() 
+
 // app.get("/",function(req, res) {
 //     res.send('Hello Express');
 //   })
 
 app.get("/json", function(req, res) {
-  res.json({"message": "Hello json"})
+  process.env.MESSAGE_STYLE == "uppercase" ? res.json({"message": "HELLO JSON"}) : res.json({"message": "Hello json"});
   })
 
 // app.use("/public", express.static(style_path))
